f34e2a07d235a9bd54373225280b548dabf23c70
Gabby <gabegabby99@gmail.com>
2021-04-08 20:33:13 -0400

    updated starter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 Function that returns info from that city
 */
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (
+    <div>
+      This is the City component
+    </div>
+  );
 }
 /*
 Function takes in Zipcode
@@ -30,6 +34,15 @@ function ZipSearchField({zipCode}) {
   );
 }
 class App extends Component {
+  /*MARK: we need to put a constructor for an array 
+  of the cities to return the multiple cities from that zipcode
+  after we create constructor, below that we are going to grab value of zipcode.
+  when we grab the value of the zipcode, we have to check if its 5 char. long 
+  */
+ //REF TO LINE 53 we have to do the same thing for another variable change in that field
+ /*REF TO LINE before 60, have to check the length of the given zip, if its  = 0 (nothing) return no results, 
+ otherwise return city component
+ */
   render() {
     return (
       <div className="App">
@@ -39,9 +52,13 @@ class App extends Component {
         <div className = "container">
           <div className = "row">
             <div className = "column">
-              <ZipSearchField />
+              <ZipSearchField 
+                //zipCode = {this.state.zipCode}
+              />
             </div>
           </div>
+
+          
           <div>
             <City />
             <City />
