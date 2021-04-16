5b892f64d9ac366d4b34a93f94d1a68a269e6b99
sngo103 <samantha.ngo04@myhunter.cuny.edu>
2021-04-16 15:36:16 -0400

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, {Component} from 'react';
-import './index.css';
-import './App.css';
-import SearchComponent from './components/SearchComponent';
-import ZipSearchField from './components/ZipSearchField';
-import City from './components/City';
+import React, { Component } from "react";
+import "./index.css";
+import SearchComponent from "./components/SearchComponent";
+import ZipSearchField from "./components/ZipSearchField";
+import City from "./components/City";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      zipcode: ''
+      zipcode: "",
     };
   }
 
@@ -18,22 +17,21 @@ class App extends Component {
   after we create constructor, below that we are going to grab value of zipcode.
   when we grab the value of the zipcode, we have to check if its 5 char. long 
   */
- //REF TO LINE 53 we have to do the same thing for another variable change in that field
- /*REF TO LINE before 60, have to check the length of the given zip, if its  = 0 (nothing) return no results, 
+  //REF TO LINE 53 we have to do the same thing for another variable change in that field
+  /*REF TO LINE before 60, have to check the length of the given zip, if its  = 0 (nothing) return no results, 
  otherwise return city component
  */
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Zip Code Search</h2>
+      <div>
+        <div className="container bg-black text-white text-center">
+          <h1 className="text-3xl p-2 m-0">Zip Code Search</h1>
+          <h3 className="p-2 m-0"> by Team Foodies </h3>
         </div>
-        <div className = "container text-center">
-          <div className = "row">
-            <div className = "column">
-              <ZipSearchField  
-                zipcode={this.state.zipcode}
-              />
+        <div className="container text-center">
+          <div className="row">
+            <div className="column">
+              <ZipSearchField zipcode={this.state.zipcode} />
             </div>
           </div>
           <div>
